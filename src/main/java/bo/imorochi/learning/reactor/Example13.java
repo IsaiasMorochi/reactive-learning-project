0409724a634fec9862351e54ed1d41766b54fbb7
imorochi <imorochi.dev@gmail.com>
2024-08-03 18:25:26 -0400

    Intervalos infinito con interval y operador el delayElements

--- src/main/java/bo/imorochi/learning/reactor/Example13.java
+++ src/main/java/bo/imorochi/learning/reactor/Example13.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
 
 public class Example13 implements ReactorExample {
 
@@ -25,5 +26,48 @@ public class Example13 implements ReactorExample {
         rango.zipWith(retraso, (ra, re) -> ra)
                 .doOnNext(i -> logger.info("doOnNext #2 - {}", i.toString()))
                 .blockLast();
+
+        try {
+            intervalDelay();
+
+            intervalInfinite();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private void intervalDelay() throws InterruptedException {
+        Flux<Integer> rango = Flux.range(1, 12)
+                .delayElements(Duration.ofSeconds(1))
+                .doOnNext(i -> logger.info(i.toString()));
+
+        rango.subscribe();
+        Thread.sleep(13000); //con fin de visualizar, retrasamos la finalizacion del programa
+
+        //rango.blockLast();//bloqueo solo para visualizar como se emite los eventos
+
+    }
+
+    private void intervalInfinite() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Flux.interval(Duration.ofSeconds(1))
+                .doOnTerminate(() -> latch.countDown()) //se ejecuta falle o no falle
+                .flatMap(i -> {
+                    if (i >= 5) {
+                        return Flux.error(new InterruptedException("Solo hasta 5!"));
+                    }
+                    return Flux.just(i);
+                })
+                .map(i -> "Hola " + i)
+                .retry(2)
+                .subscribe(
+                        s -> logger.info(s),
+                        e -> logger.error(e.getMessage()));
+
+        latch.await(); //libera cuando el latch decremente
+    }
+
 }
