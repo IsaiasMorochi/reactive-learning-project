c197e3e68545ef1db0847ffbfb347e9efe692190
imorochi <imorochi.dev@gmail.com>
2024-08-04 00:23:22 -0400

    Modo Reactive Data Driven para menejar la contrapresión

--- src/main/java/bo/imorochi/learning/web/flux/controllers/ProductoController.java
+++ src/main/java/bo/imorochi/learning/web/flux/controllers/ProductoController.java
@@ -8,28 +8,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Controller
 public class ProductoController {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+
     @Autowired
     private ProductoRepository productoRepository;
 
-    private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
 
     @GetMapping({"/listar", "/"})
     public String listar(Model model) {
 
-        Flux<Producto> productos = productoRepository.findAll().map(producto -> {
-
-            producto.setNombre(producto.getNombre().toUpperCase());
-            return producto;
-        });
+        Flux<Producto> productos = productoRepository.findAll()
+                .map(producto -> {
+                    producto.setNombre(producto.getNombre().toUpperCase());
+                    return producto;
+                });
 
         productos.subscribe(prod -> log.info(prod.getNombre()));
 
         model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de productos");
+        return "listar";
+    }
+
+    @GetMapping("/listar-datadriver")
+    public String listarDataDriver(Model model) {
+
+        Flux<Producto> productos = productoRepository.findAll()
+                .map(producto -> {
+
+                    producto.setNombre(producto.getNombre().toUpperCase());
+                    return producto;
+                })
+                .delayElements(Duration.ofSeconds(1));
+
+        productos.subscribe(prod -> log.info(prod.getNombre()));
+
+        model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 1));
         model.addAttribute("titulo", "Listado de productos");
         return "listar";
     }
