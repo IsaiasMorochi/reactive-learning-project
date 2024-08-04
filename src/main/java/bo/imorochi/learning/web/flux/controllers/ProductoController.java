79beb490253952683f2f47745ef3b9e84bb2894a
imorochi <imorochi.dev@gmail.com>
2024-08-04 00:17:14 -0400

    Creando controlador y vista reactivos

---
+++ src/main/java/bo/imorochi/learning/web/flux/controllers/ProductoController.java
@@ -0,0 +1,37 @@
+package bo.imorochi.learning.web.flux.controllers;
+
+import bo.imorochi.learning.web.flux.models.documents.Producto;
+import bo.imorochi.learning.web.flux.models.repository.ProductoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import reactor.core.publisher.Flux;
+
+@Controller
+public class ProductoController {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+
+    @GetMapping({"/listar", "/"})
+    public String listar(Model model) {
+
+        Flux<Producto> productos = productoRepository.findAll().map(producto -> {
+
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+
+        productos.subscribe(prod -> log.info(prod.getNombre()));
+
+        model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de productos");
+        return "listar";
+    }
+
+}
